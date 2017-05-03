8a6f91731c0d71876c61407eef41f9d0246fad7a
wangshuxing123 <w19850803>
2017-05-03 20:36:21 +0800

    12

--- application/controllers/address.php
+++ application/controllers/address.php
@@ -12,24 +12,41 @@ class Address extends Home_Controller{
 
 	#显示订单
 	public function show_address(){
-		$this -> output -> enable_profiler(TRUE);
-		#获取购物车数据
 		$user = $this->session->userdata('user');
 		if (empty($user)){
 			$this->load->view('login.html');
 			}
 		else{
 			$data['address'] = $this->address_model->get_address($user['user_id']);
-			$this->load->view('address.html',$data);
+			$this->load->view('address_list.html',$data);
 			}
 		
 	}
+    #   增加地址页面
+    public function add_address(){
+        $user = $this->session->userdata('user');
+        if (empty($user)){
+            $this->load->view('login.html');
+        }
+        else{
+            $query = $this->db->query('SELECT region_id, region_name FROM ci_region WHERE parent_id=?', array(0));
+            $data['province']=$query->result_array();
+            $this->load->view('address.html',$data);
+        }
+    }
+
+    public function find_citys_by_province(){
+        $provinceid = $this->input->post('provinceid',true);
+        $query = $this->db->query('SELECT region_id id, region_name name FROM ci_region WHERE parent_id=?', array($provinceid));
+        $data['success']=1;
+        $data['data']=$query->result_array();
+        echo  json_encode($data);
+    }
 
 	public function create_address(){
 		#设置验证规则
 		$user = $this->session->userdata('user');
 		$this->form_validation->set_rules('consignee','收货人姓名','trim|required');
-		var_dump($user);
 		if ($this->form_validation->run() == false) {
 			# 未通过验证
 			$data['message'] = validation_errors(); 
@@ -39,11 +56,11 @@ class Address extends Home_Controller{
 		} else {
 			# 通过验证
 			$data['user_id'] =$user['user_id'];
-			$data['consignee'] = $this->input->post('consignee',true);
+			$data['consignee'] = $this->input->post('acceptname',true);
 			$data['province'] = $this->input->post('province');
 			$data['city'] = $this->input->post('city',true);
 			$data['district'] = $this->input->post('district',true);
-			$data['street'] = $this->input->post('street',true);
+			$data['street'] = $this->input->post('addr',true);
 			$data['mobile'] = $this->input->post('mobile');
 			 var_dump($data);
 
@@ -65,10 +82,11 @@ class Address extends Home_Controller{
 		}
 		
 	}
-
-	#删除购物车信息
+	#删除地址信息
 	public function delete_address($rowid){
 		if ($this->address_model->delete_address($rowid))
 		redirect('address/show_address');
 	}
+
+
 }
